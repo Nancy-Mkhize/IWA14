23bf23ebaefd3ef3dbaeeb6a1eb8d38cc4ab939b
Nancy-Mkhize <85174921+Nancy-Mkhize@users.noreply.github.com>
2023-06-02 09:46:49 +0200

    Add files via upload

---
+++ IWA14/challenge1.js
@@ -0,0 +1,16 @@
+
+const firstName = 'John';
+const age = 35;
+const hobby = 'Coding';
+
+const logTwice = (parameter) => {
+  console.log(parameter)
+  console.log(parameter)
+}
+
+function greet () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+  return false
+}
+
+greet()
